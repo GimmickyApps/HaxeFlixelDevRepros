824f216f51ce42b27bd3a8f131adb2ebd5b3d9e0
Gimmicky Apps <gimmickyapps@gmail.com>
2016-01-20 13:22:13 +0100

    Initial check-in.

---
+++ source/PlayState.hx
@@ -0,0 +1,78 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import flixel.util.FlxDestroyUtil;
+import flixel.util.FlxMath;
+import openfl.Lib;
+using flixel.util.FlxSpriteUtil;
+
+class PlayState extends FlxState
+{
+	private var _btnPlay:FlxButton;
+	private var _backgroundColour:FlxSprite;
+	private var _logo:FlxSprite;
+	/**
+	 * Function that is called up when to state is created to set it up. 
+	 */
+	override public function create():Void
+	{
+		super.create();
+		
+		_backgroundColour = new FlxSprite();
+		add(_backgroundColour);
+		
+		_logo = new FlxSprite();
+		_logo.makeGraphic( 640, 100, FlxColor.GREEN, true );
+		FlxSpriteUtil.drawRect( _logo, 3, 3, 634, 94, FlxColor.RED );
+		add( _logo );
+		
+		_btnPlay = new FlxButton( 0, 0, "Play", clickPlay );
+		add(_btnPlay);
+		
+		onResize( Lib.current.stage.stageWidth, Lib.current.stage.stageHeight ); // do layout
+	}
+	
+	private function clickPlay():Void
+	{
+		FlxG.switchState( new PlayState() );
+	}
+	
+	/**
+	 * Function that is called when this state is destroyed - you might want to 
+	 * consider setting all objects this state uses to null to help garbage collection.
+	 */
+	override public function destroy():Void
+	{
+		super.destroy();
+		_btnPlay = FlxDestroyUtil.destroy( _btnPlay );
+		_backgroundColour = FlxDestroyUtil.destroy( _backgroundColour );
+		_logo = FlxDestroyUtil.destroy( _logo );
+	}
+
+	/**
+	 * Function that is called once every frame.
+	 */
+	override public function update():Void
+	{
+		super.update();
+	}
+	
+	override public function onResize(Width:Int, Height:Int):Void {
+		trace( Width, Height );
+		
+		_backgroundColour.makeGraphic( Width, Height, FlxColor.WHITE, true);
+		_backgroundColour.setPosition();
+		
+		_logo.setGraphicSize( Width );
+		_logo.screenCenter( true, false );
+		_logo.y = 0;
+		//_logo.y = _logo.height * ( _logo.scale.y - 0.5 );
+		
+		_btnPlay.screenCenter();
+	}
+}
